d9581e3e2841e937390e02f7018338af7b305ec0
Max Krieger <themaxaxis@gmail.com>
2016-03-07 16:33:50 -0500

    Updates

--- build.js
+++ build.js
@@ -54,9 +54,6 @@ module.exports.component = {
 		}
 	},
 
-	/**
-	 * Called once when component is attached. Generally for initial setup.
-	 */
 	init: function () {
 		this.registers = []; //order of eventing after render
 	},
@@ -65,10 +62,6 @@ module.exports.component = {
 		this.registers.push(render);
 	},
 
-	/**
-	 * Called when component is attached and when component data changes.
-	 * Generally modifies the entity based on the data.
-	 */
 	update: function (oldData) {
 		if (!oldData) this.createCanvas(this.data.width, this.data.height);
 	},
@@ -76,12 +69,16 @@ module.exports.component = {
 	createCanvas: function (w, h) {
 		var _ = this;
 		var canvas = document.createElement("canvas");
-		canvas.id = this.generateId();
 		canvas.width = w;
 		canvas.height = h;
 		canvas.style = "display: none";
 		_.canvas = canvas;
 		_.ctx = canvas.getContext("2d");
+
+		this.texture = new THREE.Texture(_.canvas); //renders straight from a canvas
+		this.el.object3D.material = new THREE.MeshBasicMaterial();
+		this.el.object3D.material.map = this.texture;
+
 		if(!this.el.hasLoaded) this.el.addEventListener("loaded", function() {
 			_.render();
 		});
@@ -97,21 +94,10 @@ module.exports.component = {
 				item();
 			});
 		}
-		this.el.setAttribute("material", "src", "url(" + this.canvas.toDataURL() + ")");
+		this.texture.needsUpdate = true;
 	},
 
-	generateId: function () {
-		var next = Math.floor(Math.random() * Math.pow(10, 20));
-		while (document.getElementById("C" + next)) {
-			next = Math.floor(Math.random() * Math.pow(10, 20));
-		}
-		return "C" + next;
-	},
-
-	/**
-	 * Called when a component is removed (e.g., via removeAttribute).
-	 * Generally undoes all modifications to the entity.
-	 */
+	//not the most removable component out there, so will leave blank for now
 	remove: function () {}
 };
 
